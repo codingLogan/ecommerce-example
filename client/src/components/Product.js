a2cc023e1b39a3d21a0bef080636c21ea18b3492
loganras <logan.razclan1@gmail.com>
2021-07-29 12:48:27 -0600

    Minor CSS adjustment for product card

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -4,7 +4,7 @@ import Rating from '../components/Rating'
 
 function Product({ product }) {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='my-3 rounded'>
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </a>
