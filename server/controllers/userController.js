88fa8fb2a3ac3eb0582669edc2a8fd91e922feca
loganras <logan.razclan1@gmail.com>
2021-08-02 11:56:38 -0600

    Add user registration endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,6 +28,37 @@ const authenticateUser = asyncHandler(async (req, res) => {
 })
 
 /**
+ * @desc Register a new user
+ * @route POST /api/users
+ * @access Public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({ name, email, password })
+  if (user) {
+    res.status(201) // something was created
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
+/**
  * @desc Get user profile
  * @route GET /api/users/profile
  * @access Private
@@ -49,4 +80,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   res.send('success')
 })
 
-export { authenticateUser, getUserProfile }
+export { authenticateUser, registerUser, getUserProfile }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,12 @@ import express from 'express'
 import {
   authenticateUser,
   getUserProfile,
+  registerUser,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.post('/login', authenticateUser)
 router.route('/profile').get(protect, getUserProfile)
 
