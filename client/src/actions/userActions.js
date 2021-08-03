eef3891f99e1f038f7f17b396ffdca975f6e545d
loganras <logan.razclan1@gmail.com>
2021-08-03 13:28:17 -0600

    Reset user specific state on logout

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -13,6 +14,7 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
+  USER_DETAILS_RESET,
 } from '../constants/userContants'
 import { buildErrorOfType } from './actionHelpers'
 
@@ -50,9 +52,9 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
-  dispatch({
-    type: USER_LOGOUT,
-  })
+  dispatch({ type: USER_LOGOUT })
+  dispatch({ type: ORDER_LIST_MY_RESET })
+  dispatch({ type: USER_DETAILS_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
