b98dbc914c15ae78a21e603d0ce97f6e693c20ef
loganras <logan.razclan1@gmail.com>
2021-08-03 14:24:01 -0600

    Add reset of data for admin vs reg users

--- client/src/pages/UserListPage.js
+++ client/src/pages/UserListPage.js
@@ -6,14 +6,21 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listUsers } from '../actions/userActions'
 
-function UserListPage() {
+function UserListPage({ history }) {
   const dispatch = useDispatch()
   const userList = useSelector((state) => state.userList)
   const { loading, error, users } = userList
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history])
 
   const deleteHandler = (id) => {
     console.log('delete')
--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -17,6 +17,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from '../constants/userContants.js'
 
 // action has type and payload
@@ -90,6 +91,8 @@ export const userListReducer = (state = { users: [] }, action) => {
       return { loading: false, users: action.payload }
     case USER_LIST_FAIL:
       return { loading: false, error: action.payload }
+    case USER_LIST_RESET:
+      return { users: [] }
     default:
       return state
   }
