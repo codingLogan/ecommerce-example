407ecf5016c8c24cf8413310ad504cf3d600328a
loganras <logan.razclan1@gmail.com>
2021-08-03 15:51:23 -0600

    Create product list page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderPage from './pages/PlaceOrderPage'
 import OrderPage from './pages/OrderPage'
 import UserListPage from './pages/UserListPage'
 import UserEditPage from './pages/UserEditPage'
+import ProductListPage from './pages/ProductListPage'
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
           <Route path='/cart/:id?' component={CartPage} />
           <Route path='/admin/userlist' component={UserListPage} />
           <Route path='/admin/user/:id/edit' component={UserEditPage} />
+          <Route path='/admin/productlist' component={ProductListPage} />
           <Route path='/' exact component={HomePage} />
         </Container>
       </main>
