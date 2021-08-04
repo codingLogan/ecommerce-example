457e94c784cbb7f3aff505b65bebdc4a21391872
loganras <logan.razclan1@gmail.com>
2021-08-04 07:27:25 -0600

    Add morgan to express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express' // ESModule
 import path from 'path'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
@@ -13,6 +14,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
