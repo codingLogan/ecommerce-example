9e49238ee84c7753e03925f8405a4ece69ace29a
loganras <logan.razclan1@gmail.com>
2021-08-04 06:50:18 -0600

    Allow upload of images from client

--- client/src/pages/ProductEditPage.js
+++ client/src/pages/ProductEditPage.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
@@ -17,6 +18,7 @@ function ProductEditPage({ match, history }) {
   const [category, setCategory] = useState('')
   const [countInStock, setCountInStock] = useState(0)
   const [description, setDescription] = useState('')
+  const [uploading, setUploading] = useState(false)
 
   const dispatch = useDispatch()
 
@@ -66,6 +68,32 @@ function ProductEditPage({ match, history }) {
     )
   }
 
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    console.log({ file })
+    const formData = new FormData()
+    formData.append('image', file)
+    setUploading(true)
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+
+      const { data } = await axios.post('/api/upload', formData, config)
+
+      console.log({ data })
+
+      setImage(data)
+      setUploading(false)
+    } catch (error) {
+      console.error(error)
+      setUploading(false)
+    }
+  }
+
   return (
     <>
       <Link to='/admin/productlist' className='btn btn-light my-3'>
@@ -111,6 +139,13 @@ function ProductEditPage({ match, history }) {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
+              <Form.File
+                id='image-file'
+                // label='Choose File'
+                custom
+                onChange={uploadFileHandler}
+              ></Form.File>
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId='brand'>
